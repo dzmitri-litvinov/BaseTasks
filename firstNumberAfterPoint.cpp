40443353e21a45e45c6ab9daa623ce5369298d4c
dzmitri-litvinov <79978383+dzmitri-litvinov@users.noreply.github.com>
2021-03-08 00:39:51 +0300

    02firstNumberAfterPoint
    3. ЦИФРА. Распечатайте первую цифру из дробной части вещественного положительного числа.

---
+++ firstNumberAfterPoint.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double number;
+	int temp;
+
+	cout << "This programm will show the first number after point in double number." << endl << endl;
+
+	cout << "Enter double: ";
+	cin >> number;
+
+	temp = number * 10;
+
+	cout << "The first number after point is " << temp % 10;
+
+	cout << endl;
+	system("pause");
+	return 0;
+}
